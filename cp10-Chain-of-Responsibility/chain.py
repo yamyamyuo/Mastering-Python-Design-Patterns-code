a38098acfd765680574ddd171a20f3b2e8e9bb01
lm <lm.91@qq.com>
2016-11-10 14:55:50 +0800

    添加了责任链的解释注释，并给出了一个被"截胡"的例子

--- cp10-Chain-of-Responsibility/chain.py
+++ cp10-Chain-of-Responsibility/chain.py
@@ -1,26 +1,29 @@
+# coding: utf-8
+
 class Event:
 
-    def __init__(self, name):
+    def __init__(self, name): # 消息初始化
         self.name = name
 
     def __str__(self):
-        return self.name
+        return self.name # str(e) 得到消息
 
 
 class Widget:
 
-    def __init__(self, parent=None):
-        self.parent = parent
+    def __init__(self, parent=None): # 初始化动态分发框架
+        self.parent = parent # 责任链的上一级对象
 
-    def handle(self, event):
-        handler = 'handle_{}'.format(event)
-        if hasattr(self, handler):
-            method = getattr(self, handler)
-            method(event)
-        elif self.parent:
-            self.parent.handle(event)
-        elif hasattr(self, 'handle_default'):
-            self.handle_default(event)
+    def handle(self, event): # 事件处理和分发和转移
+        handler = 'handle_{}'.format(event) # 事件处理函数的格式， 处理 x 事件的函数名称为 handle_x
+        if hasattr(self, handler): # 如果对象自己有该消息的处理函数
+            method = getattr(self, handler) # 将自己的消息处理函数提取为 method
+            method(event) # 执行处理函数，处理该消息，并消灭该消息(即处理完毕，不再传播)
+        elif self.parent: # 如果该对象在责任链上有上级对象
+            self.parent.handle(event) # 将消息交给上级对象处理
+        elif hasattr(self, 'handle_default'): # 如果该对象在责任链上无上级，且有默认处理函数
+            self.handle_default(event) # 对事件调用默认处理函数
+        # 若都没有处理则忽略该消息，也可传出异常？
 
 
 class MainWindow(Widget):
@@ -48,6 +51,7 @@ def main():
     mw = MainWindow()
     sd = SendDialog(mw)
     msg = MsgText(sd)
+    haha = SendDialog(msg)
 
     for e in ('down', 'paint', 'unhandled', 'close'):
         evt = Event(e)
@@ -57,6 +61,7 @@ def main():
         sd.handle(evt)
         print('Sending event -{}- to MsgText'.format(evt))
         msg.handle(evt)
+        haha.handle(evt) # 由对象 haha 处理消息 'paint' 相当于 haha 截了 sd 的胡
 
 if __name__ == '__main__':
     main()
