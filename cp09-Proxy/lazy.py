470c67d364ba473a374a5e38488cb710b3a5f068
lm <lm.91@qq.com>
2016-11-10 13:47:52 +0800

    懒代理的Python 2.7 和 Python 3 版本的不同

--- cp09-Proxy/lazy.py
+++ cp09-Proxy/lazy.py
@@ -6,14 +6,14 @@ class LazyProperty:
     def __init__(self, method):
         self.method = method
         self.method_name = method.__name__
-        # print('function overriden: {}'.format(self.method))
-        # print("function's name: {}".format(self.method_name))
+        #print('function overriden: {}'.format(self.method))
+        #print("function's name: {}".format(self.method_name))
 
     def __get__(self, obj, cls):
         if not obj:
             return None
         value = self.method(obj)
-        # print('value {}'.format(value))
+        #print('value {}'.format(value))
         setattr(obj, self.method_name, value)
         return value
 
@@ -28,17 +28,33 @@ class Test:
     @LazyProperty
     def resource(self):
         print('initializing self._resource which is: {}'.format(self._resource))
-        self._resource = tuple(range(5))    # 代价大的
+        self._resource = tuple(range(5))    # 假设这一行的计算成本比较大
         return self._resource
 
-
-def main():
+def main_Py27():
+    t = Test()
+    print(t.x)
+    print(t.y)
+    # 在 python 2.7.12 中
+    print(t.resource) # 这是一个 LazyProperty 的 instance (实例)
+    print(t.resource.__get__(t,Test)) # 调用 get 方法，将 t.resource 变为属性值
+    print(t.resource) # 再调用，得到的是 t.resource 属性值，不再计算
+    
+def main_Py3():
     t = Test()
     print(t.x)
     print(t.y)
     # 做更多的事情。。。
-    print(t.resource)
-    print(t.resource)
+    # 在 Python 3.x 中
+    print(t.resource) # 第一次调用，运行 resource() 方法，并将 t.resource 设定为其返回值
+    print(t.resource) # 再调用，得到的是 t.resource 属性值，不再计算
 
+def main():
+    import sys
+    if sys.version_info < (3,0):
+        main_Py27()
+    else:
+        main_Py3()
+        
 if __name__ == '__main__':
     main()
