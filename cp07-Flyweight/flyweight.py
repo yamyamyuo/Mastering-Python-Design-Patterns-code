a17fff283b66aa757bd4e9e563e6777269c0dd3f
yamyamyuo <nicole2014fall@163.com>
2020-07-31 12:30:40 +0800

    learn flyweight pattern

--- cp07-Flyweight/flyweight.py
+++ cp07-Flyweight/flyweight.py
@@ -1,5 +1,30 @@
 # coding: utf-8
+"""
+享元模式通过为相似对象引入数据共享来最小化内存使用, 提升性能
 
+Python中真正的构造方法是__new__,
+__init__方法做的事情是在对象创建好之后初始化变量。真正创建实例的是__new__方法。
+__new__方法用于创建对象并返回对象，当返回对象时会自动调用__init__方法进行初始化。
+
+利用python实现singleton:
+
+class Singleton(object):
+    _instance = None
+    def __new__(cls, *args, **kwargs):
+        if cls._instance is None:
+            cls._instance = object.__new__(cls, *args, **kwargs)
+
+        return cls._instance
+
+s1 = Singleton()
+s2 = Singleton()
+print(s1)
+print(s2)
+
+<__main__.Singleton object at 0x7fdef58b1190>
+<__main__.Singleton object at 0x7fdef58b1190>
+
+"""
 import random
 from enum import Enum
 
@@ -7,9 +32,11 @@ TreeType = Enum('TreeType', 'apple_tree cherry_tree peach_tree')
 
 
 class Tree:
+    # 类的属性, 所有势力共享一个变量
     pool = dict()
 
     def __new__(cls, tree_type):
+        # cls 引用的是Tree自己
         obj = cls.pool.get(tree_type, None)
         if not obj:
             obj = object.__new__(cls)
@@ -19,6 +46,7 @@ class Tree:
 
     def render(self, age, x, y):
         print('render a tree of type {} and age {} at ({}, {})'.format(self.tree_type, age, x, y))
+        print(self.__dict__)
 
 
 def main():
@@ -49,6 +77,7 @@ def main():
         tree_counter += 1
 
     print('trees rendered: {}'.format(tree_counter))
+    # 仅分配了三棵树的内存
     print('trees actually created: {}'.format(len(Tree.pool)))
 
     t4 = Tree(TreeType.cherry_tree)
